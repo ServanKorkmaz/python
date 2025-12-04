5bf5282fef54f14a66211af02711f19a7bb9da26
ServanKorkmaz <servan162@hotmail.com>
2025-12-04 12:39:13 +0100

    .

---
+++ current_date.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+def find_age(b_year, b_month, b_day):
+    today = date.today()
+    
+    age = today.year - b_year
+
+    # hvis personnen ikke har hatt bursdag i år.
+    if (today.month, today.day) < (b_month, b_day):
+        age -= 1
+
+    return age
+
+
+
+print(find_age(2002, 1,1))
+
