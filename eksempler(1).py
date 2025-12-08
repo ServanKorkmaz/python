fe920810d8ad5b895d1da2822612475f6c2a0c1b
ServanKorkmaz <servan162@hotmail.com>
2025-12-08 14:45:09 +0100

    .

--- eksempler(1).py
+++ eksempler(1).py
@@ -1,4 +1,7 @@
 
+from re import X
+
+
 def loop(a, b, c):
     x = a
     while x <= c:
@@ -9,4 +12,28 @@ def loop(a, b, c):
 # print(loop(5, 0, 10))
 # print(loop(2, -1, 5))
 # print(loop(7, 3, 6))
-print(loop(1, 5, 20))
+# print(loop(1, 5, 20))
+
+def calc(x, step, limit):
+    while x != limit:
+        x += step
+    return x
+
+# print(calc(0, 2, 10))
+# print(calc(5, -2, -5))
+# print(calc(3, 3, 5))
+# print(calc(1, 0, 1))
+
+def weird(a, b, c):
+    x = a
+    while x < c:
+        x -= b
+    return x
+
+# print(weird(10, 5, 0))
+# print(weird(1, 2, 5))
+# print(weird(-1, 0, 3))
+print(weird(0, -2, -6))
+
+
+
